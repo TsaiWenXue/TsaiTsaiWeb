947ddaa37d83f7495f0e1d13958640850412567d
TsaiWenXue <wto2005716@gmail.com>
2019-06-05 02:30:57 +0800

    add warning and modify submit button

--- static/contact.js
+++ static/contact.js
@@ -1,11 +1,18 @@
 document.getElementById("send").addEventListener('click', function(){
     var s = document.getElementById("comment").value
-    console.log(s)
-    document.getElementById("comment").value = "";
-    document.getElementById("success").style.display = "block";
+    if (s === "") {
+        document.getElementById("warning").style.display = "block";
+    }else {
+        document.getElementById("success").style.display = "block";
+        document.getElementById("comment").value = "";
+    }
+    
 });
 
-document.getElementById("close").addEventListener('click', function(){
+document.getElementById("success-close").addEventListener('click', function(){
     document.getElementById("success").style.display = "none";
 });
 
+document.getElementById("warning-close").addEventListener('click', function(){
+    document.getElementById("warning").style.display = "none";
+});
